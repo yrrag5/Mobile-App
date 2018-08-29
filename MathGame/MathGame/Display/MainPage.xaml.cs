55e3f6fba5f1a643208db067c8655134ea9dcec5
yrrag5 <G00335806@gmit.ie>
2018-08-29 23:04:29 +0100

    Added gameover and option pages

--- MathGame/MathGame/Display/MainPage.xaml.cs
+++ MathGame/MathGame/Display/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathGame.Display;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -76,7 +77,7 @@ namespace MathGame
 
         private void option_Click(object sender, RoutedEventArgs e)
         {
-
+            Frame.Navigate(typeof(Options));
         }
     }
 }
